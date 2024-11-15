14101791a2e2816ea06730c874e5d6ab71d745d5
Felipe Cruz <josepereiratuamae@gmail.com>
2024-11-15 19:33:48 -0300

    atividadeCalculadoraReact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Input from './components/input';
+import Button from './components/button'
 
-function App() {
+import { Container, Content, Row, Column } from './styles';
+import { useState } from 'react';
+
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const [firstNumber, setFirstNumber] = useState('0');
+
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+  };
+  const handleAddNumber = (number) => {
+
+    setCurrentNumber(prev => `${number} ${prev === '0' ? '': prev}`)
+  }
+
+  const handleSumNumber = () => {
+    if(firstNumber == '0'){
+      setFirstNumber(currentNumber)
+      handleOnClear();
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum))
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>  
+        <Input value={currentNumber} />
+        <Row>
+        <Button label="x"/>
+        <Button label="/"/>
+        <Button label="C" onClick={handleOnClear}/>
+        <Button label="X"/>
+        </Row>
+        <Row>
+        <Button label="7" onClick={() => handleAddNumber('7')}/>
+        <Button label="8" onClick={() => handleAddNumber('8')}/>
+        <Button label="9"onClick={() => handleAddNumber('')} />
+        <Button label="- " onClick={() => handleAddNumber('')}/>
+        </Row>
+        <Row>
+        <Button label="4" onClick={() => handleAddNumber('4')}/>
+        <Button label="5" onClick={() => handleAddNumber('5')}/>
+        <Button label="6" onClick={() => handleAddNumber('6')}/>
+        <Button label="+" onClick={handleSumNumber}/>
+        </Row>
+        <Row>
+        <Button label="1" onClick={() => handleAddNumber('1')} />
+        <Button label="2" onClick={() => handleAddNumber('2')}/>
+        <Button label="3" onClick={() => handleAddNumber('3')}/>
+        <Button label="=" onClick={() => handleAddNumber('=')}/>
+        </Row>
+      </Content>
+   
+    </Container>
   );
 }
 
